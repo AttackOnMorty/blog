d128c4a147373a6ee182e0b5008e31dbfe0e6f1b
AttackOnMorty <erenjagerm@outloook.com>
2023-12-22 14:50:15 +1000

    Clean up

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,15 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'GitHub Trends',
+    description: `Compare GitHub repos by the history of stars, issues, releases...`,
+    imgSrc: '/static/images/github-trends.png',
+    href: 'https://www.github-trends.dev/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'GitHub Ranking',
+    description: `GitHub ranking for repositories, users and organizations.`,
+    imgSrc: '/static/images/github-ranking.png',
+    href: 'https://www.github-ranking.dev/',
   },
 ]
 
