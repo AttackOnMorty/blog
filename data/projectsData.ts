85bcfd9128a0c12a6378d87f0ae33494a8d1e200
AttackOnMorty <erenjagerm@outloook.com>
2024-05-22 15:31:17 +1000

    feat: enable project page

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,26 +1,20 @@
 interface Project {
-  title: string,
-  description: string,
-  href?: string,
-  imgSrc?: string,
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
 }
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'GitHub Trends',
+    description: `Compare GitHub repositories by the history of stars, commits, releases...`,
+    href: 'https://www.github-trends.dev/',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'GitHub Ranking',
+    description: `Search GitHub's top repositories, users and organizations.`,
+    href: 'https://www.github-ranking.dev/',
   },
 ]
 
